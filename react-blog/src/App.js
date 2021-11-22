925d01dfb88cab5b13cf9d844eba1fa66778d254
qwe5507 <apvmffkdls@naver.com>
2021-11-23 00:05:36 +0900

    클릭하면 동작하는 UI (모달창) 만드는 법

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -6,6 +6,8 @@ function App() {
   let [글제목, 글제목변경] = useState(["남자 코트 추천","선릉 맛집", "회사가기 싫은 이유"]);
   let [따봉갯수, 따봉갯수변경] = useState(0);
 
+  let [modal, modal변경] = useState(false);
+
   function 제목변경하는숙제() {
     let 임시방편 = [...글제목];
     임시방편[0] = "여자 코트 추천";
@@ -38,13 +40,26 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목[2] } </h3>
+        <h3 onClick = {() => modal변경(!modal)}> { 글제목[2] } </h3>
         <p>지금 발행</p>
         <hr/>
       </div>
+      { modal
+        ? <Modal></Modal>
+        : null
+      }
 
     </div>
   );
+}
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
