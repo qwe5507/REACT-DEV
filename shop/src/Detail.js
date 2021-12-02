ec3c1f2fadf9e15e1aeb740669c4acd3aa9920dc
qwe5507 <apvmffkdls@naver.com>
2021-12-03 00:17:17 +0900

    숙제 : 자료의 순서가 변경되면 상세페이지도 고장나는 문제

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -3,17 +3,17 @@ import { useHistory, useParams } from 'react-router-dom';
 function Detail(props) {
     let history = useHistory();
     let { seq } = useParams();
-
+    let 찾은상품 = props.shoes.find(item => item.id === Number(seq));
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <img src={props.shoes.find(seq).url} width="100%" alt="" />
+                    <img src={ 찾은상품.url } width="100%" alt="" />
                 </div>
                 <div className="col-md-6 mt-4">
-                    <h4 className="pt-5">{props.shoes.find(seq).title}</h4>
-                    <p>{props.shoes.find(seq).content}</p>
-                    <p>{props.shoes.find(seq).price}</p>
+                    <h4 className="pt-5">{ 찾은상품.title }</h4>
+                    <p>{ 찾은상품.content }</p>
+                    <p>{ 찾은상품.price }</p>
                     <button className="btn btn-danger">주문하기</button>
                     <button className="btn btn-success" onClick={() => {
                         history.goBack(); 
