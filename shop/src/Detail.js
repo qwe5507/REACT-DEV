0da947f79d371f08d952e205e220bbe88c6777eb
qwe5507 <apvmffkdls@naver.com>
2021-12-03 23:36:25 +0900

    styled-components를 이용한 class없는 CSS스타일링

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,4 +1,13 @@
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let Box = styled.div`
+    padding : 20px;
+`;
+let Title = styled.h4`
+    font-size : 25px;
+    color : ${ props => props.색상 };
+`;
 
 function Detail(props) {
     let history = useHistory();
@@ -6,6 +15,10 @@ function Detail(props) {
     let 찾은상품 = props.shoes.find(item => item.id === Number(seq));
     return (
         <div className="container">
+            <Box>
+                <Title 색상="red">상세페이지</Title>
+                <Title 색상={"blue"}>상세페이지</Title>
+            </Box>
             <div className="row">
                 <div className="col-md-6">
                     <img src={ 찾은상품.url } width="100%" alt="" />
