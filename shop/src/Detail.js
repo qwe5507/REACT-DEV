0c8672d97238d2e889129a7fc01bceb2d84a7fe3
qwe5507 <apvmffkdls@naver.com>
2021-12-04 15:20:01 +0900

    Lifecycle Hook (옛날사람) useEffect (요즘사람)

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
@@ -14,6 +15,14 @@ function Detail(props) {
     let history = useHistory();
     let { seq } = useParams();
     let 찾은상품 = props.shoes.find(item => item.id === Number(seq));
+    let [alertShow, alertShow변경] = useState(false);
+
+    useEffect(()=>{
+        let 타이머 = setTimeout(()=> {alertShow변경(true)}, 2000); 
+        clearTimeout(타이머);
+        // return function 어쩌구() { 실행할 코드 }
+    });
+
     return (
         <div className="container">
             <Box>
@@ -21,9 +30,12 @@ function Detail(props) {
                 <Title 색상={"blue"}>상세페이지</Title>
                 <Title className="orange">상세페이지</Title>
             </Box>
+            { alertShow ? 
             <div className="my-alert">
                 <p>재고가 얼마 남지 않았습니다.</p>
             </div>
+            : null
+            }
             <div className="row">
                 <div className="col-md-6">
                     <img src={ 찾은상품.url } width="100%" alt="" />
